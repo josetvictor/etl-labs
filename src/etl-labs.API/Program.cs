a0ea833ec8234dda86bf9772e7fa622dd7b85393
Jose Victor <zevicctor2@gmail.com>
2024-01-28 21:43:23 -0300

    Adicionando dockerfile e docker compose

--- src/etl-labs.API/Program.cs
+++ src/etl-labs.API/Program.cs
@@ -1,10 +1,16 @@
+using System.Net.Mime;
+using System.Text.Json;
+
 using etl_labs.API.Scrapings;
+
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddHealthChecks();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSingleton<ToFiveMusicsYoutubeScraping>();
@@ -17,6 +23,21 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+
+    app.UseHealthChecks("/", new HealthCheckOptions()
+    {
+        ResponseWriter = async (context, report) =>
+        {
+            var result = JsonSerializer.Serialize(new 
+            {
+                currentTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"),
+                statusApplication = report.Status.ToString(),
+            });
+
+            context.Response.ContentType = MediaTypeNames.Application.Json;
+            await context.Response.WriteAsync(result);
+        }
+    });
 }
 
 app.UseHttpsRedirection();
